5abcc36f336851b6e5e61298391a608c7708cac5
Gil Ballesca <gilballesca@AS102776.local>
2022-07-29 21:58:06 +0800

    updates

--- src/app/week-list-selection/week-list-selection.component.ts
+++ src/app/week-list-selection/week-list-selection.component.ts
@@ -1,33 +1,63 @@
 import { Component, OnInit } from '@angular/core';
 import { HostBinding } from '@angular/core';
+import { trigger, state, animate, transition, style } from '@angular/animations'; 
 
 @Component({
   selector: 'app-week-list-selection',
   templateUrl: './week-list-selection.component.html',
   styleUrls: ['./week-list-selection.component.scss'],
+  animations: [
+    trigger('openClose', 
+    [
+      state('open', style({
+        height: '300px',
+      })),
+      state('closed', style({
+        height: '0px'
+      })),
+      transition('open => closed', [
+        animate('1s')
+      ]),
+      transition('closed => open', [
+        animate('1s')
+      ]),
+    ])
+  ]
 })
 export class WeekListSelectionComponent implements OnInit {
 
-  weekListContainerClassName: string;
+  weekListContainerClassName: {};
+  expandWeekListMenu: boolean = false;
   constructor() { }
 
   ngOnInit() {
-    this.weekListContainerClassName = "weekListContainer timesheet_div";
     this.setupContainerClass();
-
   }
 
   onArrowWeekListExpanderClick() {
+    if (this.expandWeekListMenu) {
+      this.expandWeekListMenu = false;
+    } else {
+      this.expandWeekListMenu = true;
+
+    }
     this.setupContainerClass();  
   }
 
   setupContainerClass() {
-    if (this.weekListContainerClassName === "weekListContainer timesheet_div") {
-      this.weekListContainerClassName = "weekListContainerHidden timesheet_div"; 
+    if (this.expandWeekListMenu) {
+      this.weekListContainerClassName =  {
+        weekListContainerHidden: false,
+        timesheet_div: true,
+        weekListContainer: true
+      };
     } else {
-      this.weekListContainerClassName = "weekListContainer timesheet_div";
+      this.weekListContainerClassName =  {
+        weekListContainerHidden: true,
+        timesheet_div: true,
+        weekListContainer: false
+      };
     }
     return this.weekListContainerClassName;
   }
-
 }
--- src/app/week-list-selection/week-list-selection.module.ts
+++ src/app/week-list-selection/week-list-selection.module.ts
@@ -7,6 +7,7 @@ import { IonicModule } from '@ionic/angular';
 
 import { WeekListSelectionComponent } from './week-list-selection.component';
 
+
 @NgModule({
   imports: [ CommonModule, FormsModule, IonicModule, RouterModule],
   declarations: [WeekListSelectionComponent],
